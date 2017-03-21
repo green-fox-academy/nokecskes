c67cbdf4d9c6ef8762f4d8a7b908dcbb1aafb6e6
Noemi Kecskes <nokecskes@gmail.com>
2017-03-21 12:00:57 +0100

    Hello World exercises

---
+++ week-02/day-2/src/HelloOthers.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Connor on 2017.03.21..
+ */
+public class HelloOthers {
+
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+
+        System.out.println("Hello, Nóri! \nHello, Dani! \nHello, Kristóf!");
+
+
+
+    }
+
+}
