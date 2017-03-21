5c0ca51b1c723ca972e67d52497888ac7c7c7138
Noemi Kecskes <nokecskes@gmail.com>
2017-03-21 17:53:28 +0100

    Java - Loops - first chunk of exercises

---
+++ week-02/day-2/src/DrawTriangle.java
@@ -0,0 +1,36 @@
+/**
+ * Created by Connor on 2017.03.21..
+ */
+
+import java.util.Scanner;
+
+public class DrawTriangle {
+
+    public static void main (String[] args){
+
+        // Write a program that reads a number from the standard input, then draws a
+        // triangle like this:
+        //
+        // *
+        // **
+        // ***
+        // ****
+        //
+        // The triangle should have as many lines as the number was
+
+        String asterix = "*";
+
+        System.out.println("Please give me an integer: ");
+        Scanner scanner = new Scanner(System.in);
+        int height = scanner.nextInt();
+
+        for (int i = 0; i < (height + 1); i++){
+            for (int k = 0; k < i ; k++){
+                    System.out.print(asterix);
+                }
+            System.out.print("\n");
+        }
+
+    }
+
+}
