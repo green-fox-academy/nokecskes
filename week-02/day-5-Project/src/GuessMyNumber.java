8628b4394faf24ff9b8d4d3ed2e7fdaabd376cbd
Noemi Kecskes <nokecskes@gmail.com>
2017-03-26 21:03:23 +0200

    while block added (compare user's nr with program's)

--- week-02/day-5-Project/src/GuessMyNumber.java
+++ week-02/day-5-Project/src/GuessMyNumber.java
@@ -2,8 +2,6 @@
  * Created by Connor on 2017.03.26..
  */
 
-import java.util.Scanner;
-
 /**
  * Write a program where the program chooses a number between 1 and 100.
  * The player is then asked to enter a guess.
@@ -11,6 +9,8 @@ import java.util.Scanner;
  * Make the range customizable (ask for it before starting the guessing).
  * You can add lives. (optional)
  */
+
+import java.util.Scanner;
 
 public class GuessMyNumber {
 
@@ -23,9 +23,28 @@ public class GuessMyNumber {
     int highestNr = askUserInput();
 
     int toGuess = numberGenerator(lowestNr, highestNr);
+    int lives = 5;
+    int lifeCounter = lives;
 
+    System.out.printf("I've the number between %d and %d. You have %d lives.\n", lowestNr, highestNr, lives);
 
+    do {
+      int guess = askUserInput();
+      if (guess < toGuess){
+        lives -=1;
+        System.out.printf("Too low. You have %d lives left.\n", lives);
+      }
+      else if(guess > toGuess){
+        System.out.printf("Too high. You have %d lives left.\n", lives);;
+      }
+      else{
+        System.out.printf("Congratulations. You won!\n");
+      }
+    }while(lives > 0);
 
+    if(lives == 0){
+      System.out.printf("Sorry, this time you couldn't figure it out! The number was: %d\n", toGuess);
+    }
   }
 
   public static int askUserInput(){
