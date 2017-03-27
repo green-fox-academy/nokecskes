4a2e080a737362e3d36a46224c26f3c82b340e6c
Noemi Kecskes <nokecskes@gmail.com>
2017-03-27 12:45:58 +0200

    read in lotto csv, save numbers into ArrayList

---
+++ week-03/day-1/src/Lotto.java
@@ -0,0 +1,53 @@
+/**
+ * Created by Connor on 2017.03.27..
+ */
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class Lotto {
+
+  public static void main(String[] args) {
+    // Create a method that find the 5 most common lotto numbers assets/lotto.csv
+
+    try {
+      // Reads the content from `lorem_ipsum.txt` in the `assets` folder line by line to a String List
+      Path filePath = Paths.get("assets/otos.csv");
+      List<String> lines = Files.readAllLines(filePath);
+      ArrayList<String> lottoNumbers = new ArrayList<String>();
+      HashMap<String, Integer> occurances = new HashMap<String, Integer>()
+
+      for (int i = 0; i < 3 ; i++) {
+        String oneLine = lines.get(i);
+        System.out.println(oneLine);
+        String[] columnsOfLine = oneLine.split(";");
+        for (int j = 11; j < columnsOfLine.length; j++) {
+          System.out.println(columnsOfLine[j]);
+          lottoNumbers.add(columnsOfLine[j]);
+        }
+      }
+
+      Collections.sort(lottoNumbers);
+
+      for (int i = 0; i <lottoNumbers.size() ; i++) {
+        System.out.println(lottoNumbers.get(i));
+
+
+      }
+
+
+
+
+
+    } catch (Exception e) {
+      System.out.println("Uh-oh, an error happend: " + e.getClass());
+    }
+
+  }
+
+}
