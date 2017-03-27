f4d1af53fcce10b24dc31779e75ffb3a8df7254d
Noemi Kecskes <nokecskes@gmail.com>
2017-03-27 10:47:43 +0200

    draw diagonal into square

---
+++ week-02/day-2/src/DrawDiagonal.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+/**
+ * Created by Connor on 2017.03.25..
+ */
+public class DrawDiagonal {
+
+  public static void main(String[] args) {
+    // Write a program that reads a number from the standard input, then draws a
+    // square like this:
+    //
+    //
+    // %%%%%
+    // %%  %
+    // % % %
+    // %  %%
+    // %%%%%
+    //
+    // The square should have as many lines as the number was
+
+    int height = inputNr();
+
+    String symbol = "%";
+    String space = " ";
+
+    for (int i = 0; i < height ; i++) {
+      if(i == 0 || i == (height - 1)){
+        for (int j = 0; j < height ; j++) {
+          System.out.print(symbol);
+        }
+      }
+      else{
+        for (int j = 0; j < height ; j++) {
+          if (j == 0 || j == (height - 1)){
+            System.out.print(symbol);
+          }
+          else if(j == i){
+            System.out.print(symbol);
+          }
+          else{
+            System.out.print(space);
+          }
+        }
+      }
+      System.out.println();
+    }
+  }
+
+  public static int inputNr (){
+
+    Scanner scanner = new Scanner(System.in);
+    int userGivenNr = 0;
+
+    do{
+      System.out.print("Please give me a positive number: ");
+      userGivenNr = scanner.nextInt();
+    } while (userGivenNr < 1);
+
+    return userGivenNr;
+  }
+
+
+}
