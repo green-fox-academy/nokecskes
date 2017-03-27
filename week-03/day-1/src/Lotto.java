1361f2e93038fe9afc11108cfe0589e60a99f00b
Noemi Kecskes <nokecskes@gmail.com>
2017-03-27 15:22:01 +0200

    find the minimum occurance needed

--- week-03/day-1/src/Lotto.java
+++ week-03/day-1/src/Lotto.java
@@ -6,9 +6,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import javax.xml.bind.SchemaOutputResolver;
 import sun.plugin2.message.Conversation;
 
 public class Lotto {
@@ -20,21 +22,18 @@ public class Lotto {
       // Reads the content from `lorem_ipsum.txt` in the `assets` folder line by line to a String List
       Path filePath = Paths.get("assets/otos.csv");
       List<String> lines = Files.readAllLines(filePath);
-      ArrayList<String> lottoNumbers = new ArrayList<String>();
-      HashMap<Integer, Integer> occurances = new HashMap<Integer, Integer>();
 
-      for (int i = 0; i < 3 ; i++) {
+      ArrayList<String> lottoNumbers = new ArrayList<String>();
+      for (int i = 0; i < lines.size() ; i++) {
         String oneLine = lines.get(i);
-        System.out.println(oneLine);
         String[] columnsOfLine = oneLine.split(";");
         for (int j = 11; j < columnsOfLine.length; j++) {
-          System.out.println(columnsOfLine[j]);
           lottoNumbers.add(columnsOfLine[j]);
         }
       }
-
       Collections.sort(lottoNumbers);
 
+      HashMap<Integer, Integer> occurances = new HashMap<Integer, Integer>();
       for (int i = 0; i <lottoNumbers.size() ; i++) {
         String currentToBeKey = lottoNumbers.get(i);
         int keyValue = Integer.parseInt(currentToBeKey);
@@ -46,7 +45,24 @@ public class Lotto {
         }
       }
 
-      System.out.println(occurances.get(50));
+      ArrayList<Integer> valuesFromOccurances = new ArrayList<Integer>();
+      for (int i = 0; i < occurances.size(); i++) {
+        int[] current = new int[occurances.size()];
+        valuesFromOccurances.add(i, occurances.get(i + 1));
+      }
+      Collections.sort(valuesFromOccurances);
+
+      int minimumOccurance = valuesFromOccurances.get(valuesFromOccurances.size() - 5);
+      System.out.println(minimumOccurance);
+
+      for (int i = 0; i < occurances.size(); i++) {
+        if(occurances.get(i + 1) > (minimumOccurance)){
+          System.out.println();
+        }
+      }
+
+
+
 
     } catch (Exception e) {
       System.out.println("Uh-oh, an error happend: " + e.getClass());
