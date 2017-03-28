ae4aac6fa209a7cd3430282e4214d2b821926faf
Noemi Kecskes <nokecskes@gmail.com>
2017-03-28 21:20:27 +0200

    canvas size declared as final variable, line drawer function implemented

--- week-03/day-2/src/FunctionToCenter.java
+++ week-03/day-2/src/FunctionToCenter.java
@@ -9,15 +9,23 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class FunctionToCenter {
 
+  public static final int canvasSize = 300;
+
   public static void mainDraw(Graphics graphics){
     // create a line drawing function that takes 2 parameters:
     // the x and y coordinates of the line's starting point
     // and draws a line from that point to the center of the canvas.
     // fill the canvas with lines from the edges, every 20 px, to the center.
 
+    
 
 
   }
+
+  public static void drawLineToCenter (int startX, int startY, Graphics graphics){
+    graphics.drawLine(startX, startY, canvasSize / 2, canvasSize / 2 );
+  }
+
 
   //    Don't touch the code below
   public static void main(String[] args) {
