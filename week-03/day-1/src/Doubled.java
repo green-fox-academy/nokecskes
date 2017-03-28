e5bd60d149eef2ebb6552a5aefffbfacc0887f1e
Noemi Kecskes <nokecskes@gmail.com>
2017-03-28 07:54:27 +0200

    convert each line, list of strings elements,to strings and print only the even chars

---
+++ week-03/day-1/src/Doubled.java
@@ -0,0 +1,38 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Connor on 2017.03.27..
+ */
+public class Doubled {
+
+  public static void main(String[] args) {
+    // Create a method that decrypts the assets/duplicated_chars.txt
+    try {
+      Path filePath = Paths.get("assets/duplicated-chars.txt");
+      List<String> lines = Files.readAllLines(filePath);
+
+
+      for (int i = 0; i < lines.size() ; i++) {
+        String currentLine = lines.get(i);
+        for (int j = 0; j < currentLine.length(); j++) {
+          if((j % 2) == 0){
+            System.out.print(currentLine.charAt(j));
+          }
+        }
+        System.out.println();
+      }
+
+
+    } catch (Exception e) {
+      System.out.println("Uh-oh, an error happend: " + e.getClass());
+    }
+
+
+
+  }
+
+}
