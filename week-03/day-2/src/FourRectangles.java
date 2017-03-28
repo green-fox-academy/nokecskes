61119262b6e4a85c2ee59ed736976e14de297c1e
Noemi Kecskes <nokecskes@gmail.com>
2017-03-28 12:37:31 +0200

    random size generator function added, draws 4 rectangles with for loop

--- week-03/day-2/src/FourRectangles.java
+++ week-03/day-2/src/FourRectangles.java
@@ -9,18 +9,24 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class FourRectangles {
 
-  public static void mainDraw(Graphics graphics){
+  public static void mainDraw(Graphics graphics) {
     // draw four different size and color rectangles.
 
-    for (int i = 0; i < 5 ; i++) {
-      System.out.println(randomColorGenerator());
+    for (int i = 0; i < 4 ; i++) {
+      graphics.setColor(new Color(randomColorGenerator(), randomColorGenerator(), randomColorGenerator()));
+      graphics.drawRect(randomSizeGenerator(), randomSizeGenerator(), randomSizeGenerator(), randomSizeGenerator());
     }
 
 
   }
 
-  public static int randomColorGenerator (){
+  public static int randomColorGenerator() {
     int random = (int) (Math.random() * 256);
+    return random;
+  }
+
+  public static int randomSizeGenerator() {
+    int random = (int) (Math.random() * 301);
     return random;
   }
 
