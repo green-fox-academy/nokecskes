9b4e29c444b87949ab9f90ff4578e1453e24335e
Noemi Kecskes <nokecskes@gmail.com>
2017-03-28 21:11:35 +0200

    fix canvas size and class name

---
+++ week-03/day-2/src/FunctionToCenter.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Connor on 2017.03.28..
+ */
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FunctionToCenter {
+
+  public static void mainDraw(Graphics graphics){
+    // create a line drawing function that takes 2 parameters:
+    // the x and y coordinates of the line's starting point
+    // and draws a line from that point to the center of the canvas.
+    // fill the canvas with lines from the edges, every 20 px, to the center.
+
+
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(320, 343));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
