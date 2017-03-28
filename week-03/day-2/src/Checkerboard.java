289758c72e32fc79279006f5e01f6b578d3ddeb5
Noemi Kecskes <nokecskes@gmail.com>
2017-03-28 20:46:36 +0200

    create two loops for printing even and odd lines of board

--- week-03/day-2/src/Checkerboard.java
+++ week-03/day-2/src/Checkerboard.java
@@ -7,23 +7,49 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
+final public stat
+
 public class Checkerboard {
 
   public static void mainDraw(Graphics graphics){
     // fill the canvas with a checkerboard pattern.
 
     int sizeOfSquares = 10;
+    int topleftX = 0;
+    int topleftY = 0;
 
     for (int i = 0; i < 8 ; i++) {
-      if((i % 2) == 0){
-        drawCheckLine();
+      if ((i % 2) == 0){
+        drawCheckLineOdd(sizeOfSquares, topleftX, topleftY, graphics);
+        topleftY += 10;
       }
-      
+      else{
+        drawCheckLineEven(sizeOfSquares, topleftX, topleftY, graphics);
+        topleftY += 10;
+      }
     }
-    
-    
-
   }
+
+  public static void drawCheckLineOdd(int sizeOfSquares, int startX, int startY, Graphics graphics){
+    for (int i = 0; i < 8 ; i++) {
+      graphics.drawRect(startX, startY, sizeOfSquares, sizeOfSquares);
+      if((i % 2) == 0){
+        graphics.fillRect(startX, startY, sizeOfSquares, sizeOfSquares);
+      }
+      startX +=10;
+    }
+  }
+
+  public static void drawCheckLineEven(int sizeOfSquares, int startX, int startY, Graphics graphics){
+    for (int i = 0; i < 8 ; i++) {
+      graphics.drawRect(startX, startY, sizeOfSquares, sizeOfSquares);
+      if((i % 2) != 0){
+        graphics.fillRect(startX, startY, sizeOfSquares, sizeOfSquares);
+      }
+      startX +=10;
+    }
+  }
+
 
   //    Don't touch the code below
   public static void main(String[] args) {
