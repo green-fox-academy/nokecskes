497c900616c56ca66c6a2acec403f46fb5f083d8
Noemi Kecskes <nokecskes@gmail.com>
2017-03-28 15:34:37 +0200

    fixed canvas size and class name

---
+++ week-03/day-2/src/PurpleSteps3d.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Connor on 2017.03.28..
+ */
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps3d {
+
+  public static void mainDraw(Graphics graphics){
+    // reproduce this:
+    // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/purple-steps-3d/r4.png]
+
+
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(320, 343));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
