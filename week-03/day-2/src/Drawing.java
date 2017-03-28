0ab69abde97e3bd25ad1f917517ecacd662c3d22
Noemi Kecskes <nokecskes@gmail.com>
2017-03-28 11:01:46 +0200

    draw red horizontal line to the middle

---
+++ week-03/day-2/src/Drawing.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Connor on 2017.03.28..
+ */
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+
+  public static void mainDraw(Graphics graphics){
+    //  draw a red horizontal line to the canvas' middle.
+    //  draw a green vertical line to the canvas' middle.
+
+    graphics.setColor(Color.RED);
+    graphics.drawLine(0, 150, 300, 150);
+
+    
+
+
+
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
