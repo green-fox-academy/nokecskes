2e7e410ee4ed96dd1a1530c74b0295888bef99e9
Noemi Kecskes <nokecskes@gmail.com>
2017-03-30 12:30:35 +0200

    add recursive function to replace x chars with y, add function to fix the char array to string transformation

--- week-03/day-4/src/String1.java
+++ week-03/day-4/src/String1.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -9,15 +10,42 @@ public class String1 {
     // Given a string, compute recursively (no loops) a new string where all the
     // lowercase 'x' chars have been changed to 'y' chars.
 
-    String toChange = getUserInput();
+    String input = getUserInput();
+    char[] toChangeArray = input.toCharArray();
+    char[] changedArray = changeString(toChangeArray,0);
 
+    String changed = Arrays.toString(changedArray);
+    changed = fixArrayForm(changed);
+    System.out.println(changed);
   }
 
-  public static int getUserInput() {
+  public static String getUserInput() {
     System.out.println("Give me a text with x characters: ");
     Scanner scanner = new Scanner(System.in);
     String inputString = scanner.nextLine();
     return inputString;
+  }
+
+  public static char[] changeString(char[] toChange, int index) {
+    if (index == (toChange.length - 1)) {
+      if(toChange[index] == 'x'){
+        toChange[index] = 'y';
+      }
+    }
+    else {
+      if(toChange[index] == 'x'){
+        toChange[index] = 'y';
+      }
+      changeString(toChange, index + 1);
+    }
+    return toChange;
+  }
+
+  public static String fixArrayForm(String toFix) {
+    toFix = toFix.replace("[","");
+    toFix = toFix.replace("]","");
+    toFix = toFix.replace(", ","");
+    return toFix;
   }
 
 }
