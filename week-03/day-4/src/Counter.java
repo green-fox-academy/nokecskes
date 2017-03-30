4c9342a01e107f56a27a6a0f33704e15334e30ec
Noemi Kecskes <nokecskes@gmail.com>
2017-03-30 10:20:04 +0200

    add funcion to get user input as non negative nr, add recursive function to count down from input nr

---
+++ week-03/day-4/src/Counter.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * Created by Connor on 2017.03.30..
+ */
+
+public class Counter {
+
+  public static void main(String[] args) {
+    // Write a recursive function that takes one parameter: n and counts down from n.
+
+    int countFrom = getUserInput();
+    countBack(countFrom);
+  }
+
+  public static int getUserInput() {
+    Scanner scanner = new Scanner(System.in);
+    int inputInt = 0;
+    do {
+      inputInt = scanner.nextInt();
+    }while(inputInt < 0);
+    return inputInt;
+  }
+
+  public static int countBack(int start) {
+    if(start == 0) {
+      System.out.println(0);
+    }
+    else {
+      System.out.println(start);
+      countBack(start - 1);
+    }
+    return(start);
+  }
+
+}
