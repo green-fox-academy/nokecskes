5cae6b0017d540bf754a12c4047576edec1feb05
Noemi Kecskes <nokecskes@gmail.com>
2017-03-30 11:25:28 +0200

    add function to get user input: positive int

---
+++ week-03/day-4/src/Bunny2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * Created by Connor on 2017.03.30..
+ */
+public class Bunny2 {
+
+  public static void main(String[] args) {
+    // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+    // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+    // have 3 ears, because they each have a raised foot. Recursively return the
+    // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+    int yummyBunnies = getUserInput();
+
+    System.out.println(countMultiEaredBunnies(yummyBunnies));
+
+  }
+
+  public static int getUserInput() {
+    Scanner scanner = new Scanner(System.in);
+    int inputInt = 0;
+    do {
+      System.out.println("How many bunnies do you have?");
+      inputInt = scanner.nextInt();
+    }while(inputInt < 1);
+    return inputInt;
+  }
+
+  public static int countMultiEaredBunnies(int bunnyNumber) {
+    
+  }
+
+}
