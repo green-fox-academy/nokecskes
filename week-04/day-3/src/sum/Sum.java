18a2b8710161241ec33a3319c53905d7d2340cbb
Noemi Kecskes <nokecskes@gmail.com>
2017-04-05 11:59:14 +0200

    add class method to return sum of the input elts, create example main method

---
+++ week-04/day-3/src/sum/Sum.java
@@ -0,0 +1,35 @@
+package sum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Connor on 2017.04.05..
+ */
+public class Sum {
+
+  public int sumElts(List<Integer> eltsToSum) {
+    int sum = 0;
+    for (int element : eltsToSum) {
+      sum += element;
+    }
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    ArrayList<Integer> myList = new ArrayList<>();
+    myList.add(1);
+    myList.add(3);
+    myList.add(6);
+    Sum sum = new Sum();
+    int sum = sum.sumElts(myList);
+    System.out.println(sum);
+  }
+
+
+}
+/*
+
+  Create a sum method in your class which has an ArrayList of Integers as parameter
+        It should return the sum of the elements in the list
+*/
