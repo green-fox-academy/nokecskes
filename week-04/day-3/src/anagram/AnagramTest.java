b8a6010043d492747724df9a911ef76a55e70642
Noemi Kecskes <nokecskes@gmail.com>
2017-04-05 20:34:48 +0200

    replace assertEquals with assertTrue/False

--- week-04/day-3/src/anagram/AnagramTest.java
+++ week-04/day-3/src/anagram/AnagramTest.java
@@ -10,26 +10,25 @@ import org.junit.jupiter.api.Test;
 class AnagramTest {
 
   Anagram anagramToTest = new Anagram();
-  boolean testResult = true;
   String toTest = "dog";
 
   @Test
   public void lowercaseStrings() {
-    assertEquals(testResult, anagramToTest.isAnagram(toTest, "gdo"));
+    assertTrue(anagramToTest.isAnagram(toTest, "gdo"));
   }
 
   @Test
   public void multipleCaseStrings() {
-    assertEquals(testResult, anagramToTest.isAnagram(toTest, "GDO"));
+    assertTrue(anagramToTest.isAnagram(toTest, "GDO"));
   }
 
   @Test
   public void differentLenght() {
-    assertEquals(!testResult, anagramToTest.isAnagram(toTest, "dogdog"));
+    assertFalse(anagramToTest.isAnagram(toTest, "dogdog"));
   }
 
   @Test
   public void differentStrings() {
-    assertEquals(!testResult, anagramToTest.isAnagram(toTest, "bab"));
+    assertFalse(anagramToTest.isAnagram(toTest, "bab"));
   }
 }
