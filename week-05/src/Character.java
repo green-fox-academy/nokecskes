9ad4bf83daaf632a9e8f8624d93080c478fbafb9
Noemi Kecskes <nokecskes@gmail.com>
2017-04-10 21:32:44 +0200

    add class hero, set position and image

---
+++ week-05/src/Character.java
@@ -0,0 +1,7 @@
+
+public class Character extends GameObject {
+
+  public Character(String filename, int positionX, int positionY) {
+    super(filename, positionX, positionY);
+  }
+}
---
+++ week-05/src/Hero.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Connor on 2017.04.10..
+ */
+public class Hero extends Character {
+
+  public Hero() {
+    super("assets/hero-down.png", 0, 0);
+
+  }
+
+}
