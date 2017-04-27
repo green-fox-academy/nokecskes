333ad42d8814f9fcaa169411ad2523d7c4064fa8
Noemi Kecskes <nokecskes@gmail.com>
2017-04-27 14:39:16 +0200

    add method to ask user input to get index int, fix sumArray method's exception handler's message

--- week-06/day-2/src/generics/SumArray.java
+++ week-06/day-2/src/generics/SumArray.java
@@ -1,6 +1,7 @@
 package generics;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Created by Connor on 2017.04.26..
@@ -11,11 +12,21 @@ public class SumArray {
 
     Integer[] intArray = {3, 4, 1, 2, 5};
     Double[] doubleArray = {4.5, 1.34, 1.3, 3.6, 3.654};
-    int index = 3;
+    int index = getInputIndex();
 
     Integer intArraySum = (int)sumArray(intArray, index);
     Double doubleArraySum = sumArray(doubleArray, index);
-    System.out.printf("first three int sum is %d, double sum is %f", intArraySum, doubleArraySum);
+    System.out.printf("int sum is %d, double sum is %f", intArraySum, doubleArraySum);
+  }
+
+  public static int getInputIndex () {
+    int index = 0;
+    Scanner scanner = new Scanner(System.in);
+    do {
+      System.out.println("How many elements of array do you want to add?");
+      index = scanner.nextInt();
+    } while (index < 0);
+    return index;
   }
 
   public static <T extends Number> double sumArray(T[] arrayToSum, int index) {
@@ -26,7 +37,8 @@ public class SumArray {
         sum += arrayToSum[i].doubleValue();
       }
     } catch (IndexOutOfBoundsException e) {
-      e.printStackTrace();
+      System.out.println("Index out of bound!");
+      return 0;
     }
      return sum;
   }
