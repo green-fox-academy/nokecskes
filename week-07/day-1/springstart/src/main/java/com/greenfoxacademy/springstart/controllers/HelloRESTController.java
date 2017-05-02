9164acf1cd8983917749eda63ceb1dd2407a3832
Noemi Kecskes <nokecskes@gmail.com>
2017-05-02 13:58:05 +0200

    add greet counter

--- week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloRESTController {
 
+  AtomicLong apiCalls = new AtomicLong();
+
   @RequestMapping(value = "/greeting")
   public Greeting greeting(@RequestParam("name") String name) {
-    Greeting greeting = new Greeting(1, "Hello " + name + "!");
+    Greeting greeting = new Greeting(apiCalls.incrementAndGet(), "Hello " + name + "!");
     return greeting;
   }
 
