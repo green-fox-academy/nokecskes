e82558bd17c8575ca5c1229684786a520dd670dc
Noemi Kecskes <nokecskes@gmail.com>
2017-05-02 12:57:15 +0200

    create my first spring boot application, add Hello World, add Hello World REST App and modify it to Hello User REST App

---
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Created by Connor on 2017.05.02..
+ */
+
+
+@Getter
+@AllArgsConstructor
+public class Greeting {
+
+  long id;
+  String content;
+
+}
---
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Connor on 2017.05.02..
+ */
+@RestController
+public class HelloRESTController {
+
+  @RequestMapping(value = "/greeting")
+  public Greeting greeting(@RequestParam("name") String name) {
+    Greeting greeting = new Greeting(1, "Hello " + name + "!");
+    return greeting;
+  }
+
+}
