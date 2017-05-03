c2e25a52f2b9c40be54584378872e2f9f052527e
Noemi Kecskes <nokecskes@gmail.com>
2017-05-03 14:32:05 +0200

    add animal list to controller class, add bankaccount list controller and html

--- week-07/day-2/lionkingbank/src/main/java/com/greenfoxacademy/lionkingbank/controllers/AccountController.java
+++ week-07/day-2/lionkingbank/src/main/java/com/greenfoxacademy/lionkingbank/controllers/AccountController.java
@@ -1,6 +1,8 @@
 package com.greenfoxacademy.lionkingbank.controllers;
 
 import com.greenfoxacademy.lionkingbank.models.BankAccount;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +13,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AccountController {
 
+  BankAccount simbaAccount = new BankAccount("Simba", 2000, "lion", "Zebra");
+  BankAccount timonAccount = new BankAccount("Timon", 100, "meerkat", "Leaf");
+  BankAccount zordonAccount = new BankAccount("Zordon", 0, "lion", "Zebra");
+  BankAccount zazuAccount = new BankAccount("Zazu", 8000, "bird", "Bug");
+  BankAccount pumbaAccount = new BankAccount("Pumba", 1500, "tusker", "Bug");
+
   @RequestMapping("/web/Account")
   public String addAccount(Model model) {
-    BankAccount simbaAccount = new BankAccount("Simba", 2000, "lion", "Zebra");
     model.addAttribute("account", simbaAccount);
     return "bankAccount";
+  }
+
+  @RequestMapping("/web/ListOfAccounts")
+  public String createListOfAccounts (Model model) {
+    ArrayList<BankAccount> listOfAccounts = new ArrayList<>();
+    listOfAccounts.add(simbaAccount);
+    listOfAccounts.add(timonAccount);
+    listOfAccounts.add(zordonAccount);
+    listOfAccounts.add(zazuAccount);
+    listOfAccounts.add(pumbaAccount);
+    model.addAttribute("accounts", listOfAccounts);
+    return "listOfAccounts";
   }
 
   @RequestMapping("/web/Enjoy")
