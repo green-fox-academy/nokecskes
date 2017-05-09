b944a1f65645a64fb91226f845546d3c912fc601
Noemi Kecskes <nokecskes@gmail.com>
2017-05-09 13:11:03 +0200

    listing todos from embedded database

---
+++ week-08/day-2/todoListDatabase/src/main/java/com/greenfox/TodoApplication.java
@@ -0,0 +1,33 @@
+package com.greenfox;
+
+import com.greenfox.model.Todo;
+import com.greenfox.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication implements CommandLineRunner {
+
+	@Autowired
+	TodoRepository todoRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		todoRepository.save(new Todo("I have to learn Object Relational Mapping"));
+		todoRepository.save(new Todo("Sleep"));
+		todoRepository.save(new Todo("Feed my Hippo, Fiona"));
+	}
+}
+
+
+
+	/*Inject the repository with @Autowired
+Implement CommandLineRunner run()
+				In the run() use the save() method of the repository
+				and save some new Todo("I have to learn Object Relational Mapping") i.e.*/
---
+++ week-08/day-2/todoListDatabase/src/main/java/com/greenfox/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.greenfox.repository;
+
+import com.greenfox.model.Todo;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Connor on 2017.05.09..
+ */
+public interface TodoRepository extends CrudRepository<Todo, Long> {
+}
