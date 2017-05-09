d3e456ba07b3de6d567d489e8c6ded5958365555
Noemi Kecskes <nokecskes@gmail.com>
2017-05-09 16:18:29 +0200

    fix Requestmapping under controller annotation

--- week-08/day-2/todoListDatabase/src/main/java/com/greenfox/controller/TodoController.java
+++ week-08/day-2/todoListDatabase/src/main/java/com/greenfox/controller/TodoController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Created by Connor on 2017.05.09..
  */
-@Controller("/todo")
+@Controller
+@RequestMapping("/todo")
 public class TodoController {
 
   @Autowired
