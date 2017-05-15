c35528c4f3f38a0c8e7c067f1f294532762423a2
Noemi Kecskes <nokecskes@gmail.com>
2017-05-15 12:19:23 +0200

    create tests for given parameter and no parameter cases

--- week-09/day-1/groot/src/test/java/com/greenfox/groot/controllers/GuardianControllerTest.java
+++ week-09/day-1/groot/src/test/java/com/greenfox/groot/controllers/GuardianControllerTest.java
@@ -1,13 +1,25 @@
 package com.greenfox.groot.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import com.greenfox.groot.GrootApplication;
+import java.nio.charset.Charset;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.http.MediaType;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by Connor on 2017.05.15..
@@ -16,11 +28,36 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootTest(classes = GrootApplication.class)
 @WebAppConfiguration
 @EnableWebMvc
-class GuardianControllerTest {
+public class GuardianControllerTest {
 
-  @org.junit.jupiter.api.Test
-  void translate() {
+  private MockMvc mockMvc;
 
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Before
+  public void setup() throws Exception {
+    this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
+
+  @Test
+  public void translate() throws Exception {
+    mockMvc.perform(get("/groot")
+            .param("message", "somemessage")
+            .contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.received", is("somemessage")))
+            .andExpect(jsonPath("$.translated", is("I am Groot!")))
+            .andDo(print());
+  }
+
+  @Test
+  public void noParameterException() throws Exception {
+    mockMvc.perform(get("/groot").contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.error", is("I am Groot!")));
   }
 
 }
+
