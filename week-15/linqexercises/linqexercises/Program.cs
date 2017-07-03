bc2b603c997d39b60161064413314ad074bb5925
nokecskes <nokecskes@gmail.com>
2017-07-03 13:27:55 +0200

    add gitignore, add linq exercises

---
+++ week-15/linqexercises/linqexercises/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linqexercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> evenNumbers = n.Where(i => i % 2 == 0);
+            System.Console.WriteLine("Even numbers from the list: ");
+            foreach (int i in evenNumbers)
+            {
+                System.Console.WriteLine(i);
+            }
+
+            double averageOfOddNumbers = n.Where(i => i % 2 != 0).Average();
+            System.Console.WriteLine("Average of the odd numbers: {0}", averageOfOddNumbers);
+
+            IEnumerable<int> squaredValueOfPositiveNumbers = n.Where(i => i > 0);
+            System.Console.WriteLine("Squared positive numbers from the list: ");
+            foreach (int i in squaredValueOfPositiveNumbers)
+            {
+                System.Console.WriteLine(Math.Pow(i, 2));
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
