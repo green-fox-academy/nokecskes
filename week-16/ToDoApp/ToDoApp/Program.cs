58f31ffe76b34fe277c5d7eb127d72db33b6b98e
nokecskes <nokecskes@gmail.com>
2017-07-12 17:00:56 +0200

    add last changes, not working db

---
+++ week-16/ToDoApp/ToDoApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            System.Console.WriteLine("What is the new todo?");
+            string todoDescription = Console.ReadLine();
+            Todo todo = new Todo(todoDescription);
+            */
+
+
+        }
+    }
+}
